a36137c468654cded35d3d3933df3258c7adf563
lequanphat <lequanphat3579@gmail.com>
2023-10-31 20:20:12 +0700

    add all

---
+++ app/src/main/java/com/example/contentcustomer/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.contentcustomer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        checkPermission();
+        getAllNote();
+    }
+    private void checkPermission() {
+        int readPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        int writePermission = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (readPermission != PackageManager.PERMISSION_GRANTED || writePermission != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    100);
+        }
+    }
+    @SuppressLint("Range")
+    private void getAllNote() {
+        Uri providerURI = Uri.parse("content://com.demo.note.provider/notes");
+        ContentResolver contentResolver = getContentResolver();
+        Cursor cursor = contentResolver.query(providerURI, null, null, null, null);
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                int id = cursor.getInt(cursor.getColumnIndex("id"));
+                String title = cursor.getString(cursor.getColumnIndex("title"));
+                String content = cursor.getString(cursor.getColumnIndex("content"));
+                String createdAt = cursor.getString(cursor.getColumnIndex("created_at"));
+                String updatedAt = cursor.getString(cursor.getColumnIndex("updated_at"));
+                Log.i("NOTHING", title);
+            }
+            cursor.close();
+        }
+        Log.i("NOTHING", "abc");
+    }
+}
